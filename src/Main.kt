c91e07890c196e50e20354feaf74894cc6f2ccbe
kimura-khamali <kimurakhamali@gmail>
2024-02-16 22:41:38 +0300

    added file

---
+++ src/Main.kt
@@ -0,0 +1,141 @@
+/*fun main() {
+   val firstName ="Jack"
+   val lastName = " Block"
+   val fullName = firstName +" " + lastName
+   println(fullName)
+}*/
+/*fun main(){
+   var num1 = 45
+   var num2 = 78
+   var result = num1 + num2
+   println(result)
+}*/
+/*fun main(){
+   var num1 = 108
+   var num2 = 40
+   var result = num1 - num2
+   println(result)
+   }*/
+
+/*fun main() {
+   var num1 = 108
+   var num2 = 40
+   var result = num1 * num2
+   println(result)
+}*/
+   /*fun main() {
+   var num1 = 78
+   var num2 = 45.0
+   var result = num1 / num2
+   println(result)
+}*/
+
+/*fun main() {
+   var num1 = 45
+   var num2 = 78
+   var result = num1 % num2
+   println(result)
+}*/
+
+/*fun main() {
+   var a = 2
+   var b = 3
+   a += b
+   println(a)
+}*/
+
+/*fun main() {
+   var a = 2
+   var b = 3
+   a -= b
+   println(a)
+}*/
+
+/*fun main() {
+   var a =3
+   var x = a++
+
+   println(a)
+   print(x)
+}*/
+
+/*fun main() {
+   var a = 3
+   var x = a--
+
+   println(x)
+   print(a)
+}*/
+
+/*fun main() {
+   var a = 3
+   var b = 4
+
+   //println(a > b)
+   //println(a<b)
+     println(a<=b)
+}*/
+
+/*fun main() {
+   var a = 3
+   var b = 4
+
+   print(a == b)
+}*/
+
+/*fun main() {
+   var a = 3
+   var b = 4
+
+
+   print(a != b)
+}*/
+/*fun main(){
+   printMyName()
+}
+
+fun printMyName(){
+   println("Brenda Khamali")
+}*/
+
+/*fun main(){
+   modulus()
+}
+
+fun printMyName(){
+   println("Adalab 2024")
+}
+fun modulus(){
+  var num1 = 80
+   var num2 =38
+   var results= num1 % num2
+
+   println(results)
+}*/
+
+fun main(){
+   modulus(num1 =80 , num2 = 38)
+   countCharacter()
+
+   addtwoNumbers(num1=60, num2=51)
+}
+
+fun printMyName(){
+   println("Adalab 2024")
+}
+fun modulus(num1: Int, num2: Int){
+   var results= num1 % num2
+   println(results)
+}
+
+fun countCharacter(){
+   var word = "Avacado"
+   var wordLen = word.length
+   println(wordLen)
+}
+
+fun addtwoNumbers(num1: Int, num2: Int){
+   var sum =num1 + num2
+   println(sum)
+}
+
